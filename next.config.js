89c5a7d139765a28388e44f2a1161da5268fdad3
Ibrahim-Furkan Demirbilek <ibrahimdmrblk@outlook.de>
2021-04-22 21:32:07 +0200

    fixing mistake

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ module.exports = {
     domains: ['images.ctfassets.net'],
   },
   env: {
-    NEXT_SERVICE_ID: process.env.SERVICE_ID,
-    NEXT_TEMPLATE_ID: process.env.TEMPLATE_ID,
-    NEXT_USER_ID: process.env.USER_ID,
+    NEXT_SERVICE_ID: process.env.NEXT_SERVICE_ID,
+    NEXT_TEMPLATE_ID: process.env.NEXT_TEMPLATE_ID,
+    NEXT_USER_ID: process.env.NEXT_USER_ID,
   },
 };
